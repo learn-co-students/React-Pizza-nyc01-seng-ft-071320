bebec554eac0d65c3bfbfb1e2e9f296d9110eab6
victorscholz <vgscholz@gmail.com>
2020-09-21 16:51:15 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
-import React, { Component, Fragment } from 'react';
-import Header from './components/Header'
-import PizzaForm from './components/PizzaForm'
-import PizzaList from './containers/PizzaList'
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
+import PizzaForm from "./components/PizzaForm";
+import PizzaList from "./containers/PizzaList";
+
 class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Header/>
-        <PizzaForm/>
-        <PizzaList/>
-      </Fragment>
-    );
-  }
+	state = {
+		pizzaArray: [],
+	};
+
+	componentDidMount() {
+		fetch("http://localhost:3000/pizzas/")
+			.then((response) => response.json())
+			.then((data) => this.setState({ pizzaArray: data }));
+	}
+
+	newPizza = (pizzaObj) => {
+		let newPizzaObj = pizzaObj;
+
+		const configObj = {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+				Accepts: "application/json",
+			},
+			body: JSON.stringify(newPizzaObj),
+		};
+		fetch("http://localhost:3000/pizzas/", configObj)
+			.then((resp) => resp.json())
+			.then((data) => console.log(data));
+	};
+
+	editHandler = (pizza) => {
+		const configObj = {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+				Accepts: "application/json",
+			},
+			body: JSON.stringify({ pizza }),
+		};
+		fetch(`http://localhost:3000/pizzas/${pizza.id}`, configObj)
+			.then((response) => response.json())
+			.then((updatedPizza) => {
+				console.log(updatedPizza);
+			});
+	};
+
+	render() {
+		return (
+			<Fragment>
+				<Header />
+				<PizzaForm submitForm={this.newPizza} />
+				<PizzaList
+					editHandler={this.editHandler}
+					pizzaArray={this.state.pizzaArray}
+				/>
+			</Fragment>
+		);
+	}
 }
 
 export default App;
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
-import Pizza from '../components/Pizza'
-class PizzaList extends Component {
+import React, { Component } from "react";
+import Pizza from "../components/Pizza";
 
-  render() {
-    return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Topping</th>
-            <th scope="col">Size</th>
-            <th scope="col">Vegetarian?</th>
-            <th scope="col">Edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            //render Pizza here
-          }
-        </tbody>
-      </table>
-    );
-  }
-
-}
+const PizzaList = (props) => {
+	return (
+		<table className="table table-striped">
+			<thead>
+				<tr>
+					<th scope="col">Topping</th>
+					<th scope="col">Size</th>
+					<th scope="col">Vegetarian?</th>
+					<th scope="col">Edit</th>
+				</tr>
+			</thead>
+			<tbody>
+				{props.pizzaArray.map((element) => (
+					<Pizza
+						pizza={element}
+						key={element.id}
+						editHandler={props.editHandler}
+					/>
+				))}
+			</tbody>
+		</table>
+	);
+};
 
 export default PizzaList;
