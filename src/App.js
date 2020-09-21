f38455c48ba06645962dc3e388deafffd1438b99
irosen419 <irosen419@gmail.com>
2020-09-21 15:01:35 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,56 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+
+  state = {
+    pizzaArray: [],
+    currentPizza: ""
+  }
+
+  componentDidMount() {
+    this.fetchApi()
+  }
+
+  fetchApi = () => {
+    fetch('http://localhost:3000/pizzas')
+      .then(resp => resp.json())
+      .then(pizzas => this.setState(() => ({ pizzaArray: pizzas })))
+  }
+
+  getPizza = (pizza) => {
+    this.setState({ currentPizza: pizza })
+  }
+
+  patchPizza = (pizza) => {
+    const pizzaObj = {
+      topping: pizza.topping,
+      size: pizza.size,
+      vegetarian: pizza.vegetarian
+    }
+    console.log(pizza, pizzaObj)
+
+    const options = {
+      method: 'PATCH',
+      headers: {
+        "Content-type": "application/json",
+        "Accepts": "application/json"
+      },
+      body: JSON.stringify(pizzaObj)
+    }
+
+    fetch(`http://localhost:3000/pizzas/${pizza.id}`, options)
+      .then(resp => resp.json())
+      .then(newPizza => this.fetchApi())
+
+    this.setState(() => ({ currentPizza: "" }))
+  }
+
   render() {
     return (
       <Fragment>
-        <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <Header />
+        <PizzaForm pizza={this.state.currentPizza} patchPizza={this.patchPizza} />
+        <PizzaList pizzas={this.state.pizzaArray} getPizza={this.getPizza} />
       </Fragment>
     );
   }
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,12 +1,16 @@
 import React from "react"
 
-const Pizza = () => {
-  return(
+const clickHandler = (e, props) => {
+  props.getPizza(props.pizza)
+}
+
+const Pizza = (props) => {
+  return (
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
-      <td><button type="button" className="btn btn-primary">Edit Pizza</button></td>
+      <td>{props.pizza.topping}</td>
+      <td>{props.pizza.size}</td>
+      {props.pizza.vegetarian ? <td>Yes</td> : <td>No</td>}
+      <td><button type="button" className="btn btn-primary" onClick={(e) => clickHandler(e, props)}>Edit Pizza</button></td>
     </tr>
   )
 }
