122dacc580d6241201ea0abc1eac0970b2805cde
pairofAces <karanchauhan16@gmail.com>
2020-09-21 23:13:05 -0400

    Done.

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,14 +1,22 @@
 import React from "react"
 
-const Pizza = () => {
+//create the clickHandler, since this is a function instead of a class, I'll have to invoke an 
+//anonymous function within the clickHandler so that it will ONLY INVOKE ON A CLICK
+const clickHandler = (e, props) => {
+  props.getPizza(props.pizza)
+}
+
+//give props to the function to accept as an argument, to inherit the necessary info
+const Pizza = (props) => {
   return(
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
-      <td><button type="button" className="btn btn-primary">Edit Pizza</button></td>
+      <td>{props.pizza.topping}</td>
+      <td>{props.pizza.size}</td>
+      <td>{props.pizza.vegetarian ? <td>Yes</td> : <td>No</td>}</td>
+      <td><button type="button" className="btn btn-primary" onClick={(e) => clickHandler(e, props)}>Edit Pizza</button></td>
     </tr>
   )
 }
+
 
 export default Pizza
